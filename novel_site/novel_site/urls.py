b49983ead8b53dafe17e1b48789ad9ecde74f5b5
ningyixiang <386741589@qq.com>
2020-02-29 23:36:23 +0800

    创建login相关

--- novel_site/novel_site/urls.py
+++ novel_site/novel_site/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from login.views import login, register
 
 urlpatterns = [
     path('books/', include('book.urls')),
     path('admin/', admin.site.urls),
+    path('login/', login),
+    path('register/', register)
 ]
